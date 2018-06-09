4b06e49e5593f4710be4291eb20c625d90dbd5ba
Justin Albano <albano.justin@gmail.com>
2018-06-09 08:57:42 -0400

    Added functionalty to update a budget item.

--- lockdown-core/src/main/java/com/lockdown/rest/controller/BudgetItemsController.java
+++ lockdown-core/src/main/java/com/lockdown/rest/controller/BudgetItemsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,5 +69,11 @@ public class BudgetItemsController {
 		else {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		}
+	}
+	
+	@PutMapping("/item/{itemId}")
+	public ResponseEntity<BudgetItemResource> updateBudgetItems(@PathVariable String budgetId, @PathVariable String itemId, @RequestBody BudgetItem body) {
+		BudgetItem updatedBudgetItem = budgetItemDataStore.saveAndCascade(body);
+		return new ResponseEntity<>(budgetItemAssembler.toResource(updatedBudgetItem), HttpStatus.OK);
 	}
 }
